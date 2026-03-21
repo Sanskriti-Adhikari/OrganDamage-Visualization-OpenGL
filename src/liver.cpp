dca9e7a56203363c99fcdac30c4ae7810ec34671
tejaswi acharya <tejaswiacharya21@gmail.com>
2026-03-21 13:16:37 +0545

    lungs-damage-initial-commit

---
+++ src/liver.cpp
@@ -0,0 +1,2 @@
+/* liver.cpp — to be implemented by Tejaswi Acharya (081BCT088) */
+#include "liver.h"
---
+++ src/liver.h
@@ -0,0 +1,21 @@
+/*
+ * liver.h  —  LiverSimulation stub
+ * To be implemented by Tejaswi Acharya (081BCT088).
+ *
+ * Instructions:
+ *   1. Implement all methods in liver.cpp
+ *   2. Recompile — main.cpp already routes to this class
+ *
+ * Tribhuvan University – Organ Damage Visualization
+ */
+#pragma once
+
+class LiverSimulation {
+public:
+    void reset()              {}
+    void update(float /*dt*/) {}
+    void render()             {}
+    void onKeyRight()         {}
+    void onKeyLeft()          {}
+    void toggleAuto()         {}
+};
